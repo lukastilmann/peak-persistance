715f156e5cfa7b024c155cf04ad724f7cf5b11f9
LNhart <lukastilmann97@gmail.com>
2025-02-17 19:44:29 +0100

    Removing adding back of centroid, which is unneccesary when using time_warping function

--- R/align_functions.R
+++ R/align_functions.R
@@ -26,29 +26,9 @@ align_functions <- function(fun_curves, lambda = 0.0, parallel = FALSE,
     data_aligned_tw <- time_warping(curves_arr_tw, t_grid, lambda = lambda,
                                     parallel = TRUE, max_iter = 20)
 
-    # Get the initial values of original functions
-    f0_init <- data_aligned_tw$f0[1,]
-    #View(data_aligned_tw)
-
-    # Calculate the mean/median that was used
-    if(data_aligned_tw$call$centroid_type == "mean") {
-      center_value <- mean(f0_init)
-    } else {
-      center_value <- median(f0_init)
-    }
-
     # Get the aligned functions
     fn <- data_aligned_tw$fn
-
-    # Recover each original function
-    recovered <- matrix(0, nrow=nrow(fn), ncol=ncol(fn))
-    for(i in 1:ncol(fn)) {
-      # Add back the difference in initial values
-      offset <- f0_init[i] - center_value
-      recovered[,i] <- fn[,i] + offset
-    }
-
-    result <- t(recovered)
+    result <- t(fn)
   } else {
     print("csa for curve_srvf_align or tw for time_warping implemented")
   }
@@ -56,3 +36,4 @@ align_functions <- function(fun_curves, lambda = 0.0, parallel = FALSE,
   aligned_curves <- tfd(result, arg = t_grid)
   return(aligned_curves)
 }
+
